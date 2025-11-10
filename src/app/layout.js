139021cc1b8dbebe5bfb2d7c481b1e69e13075b8
yanzhi <yanzhi@enso-ai.com>
2025-11-10 14:27:46 -0800

    Add version tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,27 @@
 'use client';
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from 'next/font/google';
 import { AuthProvider } from 'authManager/authContext';
-import "./globals.css";
+import VersionDisplay from 'components/common/VersionDisplay';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
 });
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={`${geistSans.variable} ${geistMono.variable}`}>
+                <AuthProvider>{children}</AuthProvider>
+                <VersionDisplay />
+            </body>
+        </html>
+    );
 }
