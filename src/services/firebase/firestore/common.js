b140c6aff8072aa38a1b6c73815f917d08ee3f78
Jimmy <yanzhi@enso-ai.com>
2025-11-12 13:26:50 -0800

    clear constants for the old collection path

--- src/services/firebase/firestore/common.js
+++ src/services/firebase/firestore/common.js
@@ -1,6 +1,3 @@
-export const OLD_DATABASE_ID = 'shorts-factory-projects';
-export const OLD_COLLECTION_VIDEOS = 'posting-videos'; // todo change it to videos later
-
 export const DATABASE_ID = 'shorts-hub';
 export const COLLECTION_VIDEOS = 'videos'; // todo change it to videos later
 export const COLLECTION_CHANNELS = 'channels';
